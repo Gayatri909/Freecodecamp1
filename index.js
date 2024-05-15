49f484467731e69056c796132c811f2c7537a963
alwala gayatri goud <143960921+Gayatri909@users.noreply.github.com>
2024-05-15 20:30:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//document.getElementById("counter-el").innerText=5;
+let countEl = document.getElementById("count-el");
+let saveBtn = document.getElementById("save-btn");
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment () {
+    count += 1
+    countEl.textContent = count
+};
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+};
